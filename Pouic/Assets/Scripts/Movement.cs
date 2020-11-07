8278ed8213ffa0ab0ac1f5b3d5eb6156600c4a06
Corentin Gautier <36226516+Adorr29@users.noreply.github.com>
2020-11-07 23:02:09 +0100

    Add basic Movement

---
+++ Pouic/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+
+    Rigidbody rb;
+
+    [HideInInspector]
+    public Vector3 direction = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(direction * speed, ForceMode.Acceleration);
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        //rb.AddForce(-rb.velocity * acceleration * 2);
+    }
+    */
+}
---
+++ Pouic/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Movement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3();
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        movement.direction = direction;
+    }
+}
