c0bbccfe7004de605c6fb6c2191bc7bff1b43ebc
Corentin Gautier <36226516+Adorr29@users.noreply.github.com>
2020-11-10 21:16:54 +0100

    Head look at mouse position

--- Pouic/Assets/Scripts/Player.cs
+++ Pouic/Assets/Scripts/Player.cs
@@ -2,11 +2,12 @@
 
 public class Player : MonoBehaviour
 {
+    public new Camera camera;
     public Transform headTransform;
 
     Movement movement;
 
-    public float forfun = 0;
+    public float headRotationSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,24 +18,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* // Global
-        Vector3 direction = new Vector3();
-
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.z = Input.GetAxisRaw("Vertical");
-
-        direction.Normalize();
-
-        movement.direction = direction;
-        */
-
-        // Local
+        // Move
         float forward = Input.GetAxisRaw("Vertical");
         float torque = Input.GetAxisRaw("Horizontal");
 
         movement.direction = transform.forward * forward;
         movement.rotateDirection = torque;
 
-        headTransform.Rotate(new Vector3(0, forfun, 0) * Time.deltaTime);
+        // Head Rotate
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 lookDirection = (hit.point - transform.position).normalized;
+            float angleY = -Mathf.Atan2(lookDirection.z, lookDirection.x) * Mathf.Rad2Deg;
+            Quaternion angle = Quaternion.Euler(new Vector3(0f, angleY, 0f));
+
+            // TODO use localRotation
+            headTransform.rotation = Quaternion.Lerp(headTransform.rotation, angle, headRotationSpeed * Time.deltaTime); // or we can use Quaternion.RotateTowards
+        }
+
+        // Camera Zoom
+        camera.transform.Translate(new Vector3(0, 0, Input.mouseScrollDelta.y), Space.Self);
+    }
+
+    Vector3 GetLookDirection()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+            return (hit.point - transform.position).normalized;
+
+        return Vector3.zero;
     }
 }
